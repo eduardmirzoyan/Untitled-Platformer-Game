78493c761861620b3c9ead5fa421ea437b783d66
Eduard Mirzoyan <edd@berkeley.edu>
2023-01-13 18:46:35 -0800

    Fixed other changes

--- Assets/Scripts/Enemy/BatController.cs
+++ Assets/Scripts/Enemy/BatController.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.AI;
-using UnityEngine.UI;
+using NavMeshPlus;
 
 public class BatController : MonoBehaviour
 {
